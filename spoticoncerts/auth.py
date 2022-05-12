3314b6817d41bb9762a4bfee7fa74a5553859ddc
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2022-05-11 23:56:45 -0700

    Create request form for register()

--- spoticoncerts/auth.py
+++ spoticoncerts/auth.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 #Set up blueprint for Flask application
 auth = Blueprint('auth', __name__)
 
-@auth.route('/register')
+@auth.route('/register', methods = ['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+    else:
+        return render_template('register.html')
 
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template('login.html')
