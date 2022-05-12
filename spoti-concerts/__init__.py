443f6a0f0195345cd088d088e91e07cc196127d3
Sebastian Alvarez <32760814+sebasalvarez13@users.noreply.github.com>
2022-05-11 21:36:40 -0700

    Delete spoti-concerts directory

--- spoti-concerts/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def creat_app():
-    app = Flask(__name__)
-    #Define secret key to encrypt/secure cookies and session data
-    app.config['SECRET_KEY'] = "asdfghjkl"
-
-    return app
