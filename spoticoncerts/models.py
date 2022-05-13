5f51e2ffb9ad5d32f75ea270722b2d126b6f183e
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2022-05-12 23:49:49 -0700

    Create Song class

--- spoticoncerts/models.py
+++ spoticoncerts/models.py
@@ -1,8 +1,10 @@
 '''Here is where we create our database models'''
 
+from pytz import timezone
 from . import db
 #UserMixin is a custom class that will give our user object specific things for our login
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class User(db.model, UserMixin):
     #Define the columns for our table
@@ -12,4 +14,12 @@ class User(db.model, UserMixin):
     email = db.Column(db.String(100), unique = True)
     username = db.Column(db.String(100), unique = True)
     password = db.Column(db.String(100))
+    songs = db.relationship('Songs')
 
+class Song(db.model):
+    id = db.column(db.Integer, primary_key = True)
+    song = db.Column(db.String(100))
+    artist = db.Column(db.String(100))
+    album = db.Column(db.String(100))
+    played_at = db.Column(db.DateTime(timezone = True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id')) 
